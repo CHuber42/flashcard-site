dc75e7f306112710f213bde8da2780020f23cdbc
Allison Mackey <a.mackey693@gmail.com>
2020-03-24 15:00:21 -0700

    adds html, javascript & jquery to our web page dom-manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
   });
 
   $("button#hello").click(function() {
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").after("<li>Hello!</li>");
+    $("ul#webpage").after("<li>Why hello there!</li>");
     
     $("ul#user").children("li").first().click(function(){
       $(this).remove();
@@ -18,8 +18,8 @@ $(document).ready(function() {
   });
 
   $("button#goodbye").click(function() {
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").before("<li>Goodbye!</li>");
+    $("ul#webpage").before("<li>Goodbye, dear user!</li>");
     $("ul#user").children("li").first().click(function(){
       $(this).remove();
     });
@@ -30,8 +30,8 @@ $(document).ready(function() {
   });
 
   $("button#stop").click(function() {
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").append("<li>Stop copying me!</li>");
+    $("ul#webpage").append("<li>Pardon me. I meant no offense.</li>");
     $("ul#user").children("li").first().click(function(){
       $(this).remove();
     });
@@ -40,5 +40,7 @@ $(document).ready(function() {
       $(this).remove();
     });
   });
+
+  $("#salad")
 
 });
