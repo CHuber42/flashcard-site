39e7ae43c4ce3853e3c5815c78f4af9f59607e3d
Allison Mackey <a.mackey693@gmail.com>
2020-03-24 14:07:34 -0700

    adds additional web page. initial html, css & javascript to said webpage

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,16 @@ $(document).ready(function() {
     $(this).children("p").slideToggle();
   });
 
+  $("button#hello").click(function() {
+    $("ul").prepend("<li>Hello!</li>");
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul").prepend("<li>Goodbye!</li>");
+  });
+
+  $("button#stop").click(function() {
+    $("ul").prepend("<li>Stop copying me!</li>");
+  });
 
 });
